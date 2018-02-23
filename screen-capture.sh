702ba9d906ba7b9a02324d95e9534854081ec22e
Masayuki Kawamura <rvillage@gmail.com>
2018-02-23 21:53:55 +0900

    Update screen-capture.sh

--- screen-capture.sh
+++ screen-capture.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-google-chrome --headless --disable-gpu --screenshot --window-size=800,1200 http://sekai-kabuka.com/bitcoin.html
-google-chrome --headless --disable-gpu --screenshot --window-size=800,1200 http://sekai-kabuka.com/bitcoin.html
-google-chrome --headless --disable-gpu --screenshot --window-size=800,1200 http://sekai-kabuka.com/bitcoin.html
-google-chrome --headless --disable-gpu --screenshot --window-size=800,1200 http://sekai-kabuka.com/bitcoin.html
+google-chrome --headless --disable-gpu --dump-dom http://sekai-kabuka.com/bitcoin.html
+google-chrome --headless --disable-gpu --print-to-pdf http://sekai-kabuka.com/bitcoin.html
+google-chrome --headless --disable-gpu --screenshot http://sekai-kabuka.com/bitcoin.html
 google-chrome --headless --disable-gpu --screenshot --window-size=800,1200 http://sekai-kabuka.com/bitcoin.html
 convert screenshot.png -crop '755x630+20+400' image.jpg
